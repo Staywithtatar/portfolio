3e83acd9987eb7458aac85cbaeb1be3dc17e51bd
Staywithtatar <154976428+Staywithtatar@users.noreply.github.com>
2025-04-16 04:23:19 +0700

    อัพเดตเพิ่มเติมส่วนโปรไฟล์

--- src/components/profile/ProfileCard.js
+++ src/components/profile/ProfileCard.js
@@ -8,25 +8,25 @@ export default function ProfileCard() {
   const { t } = useLanguage();
   
   return (
-    <div className="bg-gray-900/30 backdrop-blur-md p-6 flex flex-col border border-white/10"
+    <div className="bg-gray-900/30 backdrop-blur-md p-4 flex flex-col border border-white/10 h-full"
       style={{ 
         borderRadius: '60px 20px 40px 20px',
         boxShadow: '0 8px 32px rgba(31, 38, 135, 0.15)'
       }}>
-      <div className="mb-4">
-        <div className="overflow-hidden mb-4"
-          style={{ borderRadius: '50px 15px 30px 15px' }}>
-          <Image
-            src="/image/profile2.jpg"
-            alt="Profile"
-            width={500}
-            height={500}
-            className="w-full object-cover transform hover:scale-105 transition duration-500"
-            priority
-          />
-        </div>
-        <h2 className="text-2xl font-bold text-white mb-1">{t('name')} <span className="text-yellow-400">✋</span></h2>
-        <p className="text-gray-300 mb-4">
+      <div className="mb-3">
+      <div className="overflow-hidden mb-3 max-w-[350px] mx-auto"
+  style={{ borderRadius: '50px 15px 30px 15px' }}>
+  <Image
+    src="/image/profile2.jpg"
+    alt="Profile"
+    width={350}  
+    height={350}
+    className="w-full h-[230px] object-cover object-center transform hover:scale-105 transition duration-500"
+    priority
+  />
+</div>
+        <h2 className="text-xl font-bold text-white mb-1">{t('name')} <span className="text-yellow-400">✋</span></h2>
+        <p className="text-l text-gray-300 mb-4">
           {t('intro')} <span className="text-white">🚀</span>
         </p>
       </div>
